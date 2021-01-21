2891edb14f97a65d7049b78c233a264eb4fca3ca
eyesack00 <77690918+eyesack00@users.noreply.github.com>
2021-01-20 18:00:30 -0600

    Update LoadReader.py

--- LoadReader.py
+++ LoadReader.py
@@ -94,7 +94,7 @@ try:
         #value = random.randint(100)
         measure_time = time.perf_counter()
     
-        print(counter, (value-29800)/10000, measure_time-start)
+        print(counter, value, measure_time-start)
         file.write(str(counter) +  " " + str(value) + " " + str(measure_time-start) + "\n") #write counter, value, and time after start
         x.append(measure_time-start)
         y.append(value)
